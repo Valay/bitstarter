6b75e792fcd1f4340753639d6734a10c5e0fdc38
Valay <valayshah26@gmail.com>
2013-08-12 16:28:22 -0400

    Web.js modified

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World22!');
+  	var buf = fs.readFileSync('index.html');
+	var data = buf.toString();
+	response.send(data);
 });
 
 var port = process.env.PORT || 5000;
